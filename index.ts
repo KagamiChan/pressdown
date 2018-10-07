4c1d4f3657709ced4d23f25ad2163b1f28235b66
Kagami <moe.pascal@gmail.com>
2018-10-08 00:20:14 +0800

    feat: conversion

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 import Promise from 'bluebird'
+import dateFns from 'date-fns'
 import fs from 'fs-extra'
+import got from 'got'
+import matter from 'gray-matter'
+import { each, filter } from 'lodash'
+import path from 'path'
+import TurndownService from 'turndown'
+import url from 'url'
 import convert, { ElementCompact } from 'xml-js'
 
 interface ITextType {
@@ -16,20 +23,73 @@ interface IWordpressPost {
   'content:encoded': ICDataType
   'wp:post_type': ICDataType
   'wp:status': ICDataType
+  'wp:post_name': ICDataType
 }
 
 interface IWordpressDataCompact extends ElementCompact {
   rss: {
     channel: {
-      item: []
+      item: IWordpressPost[]
     }
   }
 }
+
+const turndownService = new TurndownService()
+const markdownImgaeRegex = /(?:!\[(.*?)\]\((.*?)\))/gm
 
 const main = async () => {
   const xml = await fs.readFile(process.argv[2])
 
   const data = convert.xml2js(xml.toString(), { compact: true }) as IWordpressDataCompact
+
+  const posts: IWordpressPost[] = filter(
+    data.rss.channel.item,
+    (item: IWordpressPost) => item['wp:post_type']._cdata === 'post' && Boolean(item['content:encoded']._cdata),
+  )
+
+  const postsPath = path.resolve(__dirname, 'posts')
+
+  await Promise.map(posts, async (post: IWordpressPost) => {
+    const date = dateFns.parse(post['wp:post_date']._cdata)
+    let text = turndownService.turndown(post['content:encoded']._cdata)
+    const title = decodeURI(post['wp:post_name']._cdata || post.title._text)
+    const directory = path.join(postsPath, `${dateFns.format(date, 'YYYY-MM-DD')}-${title}`)
+
+    await fs.ensureDir(directory)
+
+    const files: { [key: string]: string } = {}
+
+    text = text.replace(markdownImgaeRegex, (match: string, p1: string, p2: string) => {
+      const uri = p2.split(' ')[0]
+      const parsed = url.parse(uri)
+      const filename = path.basename(parsed.pathname || '')
+      if (!filename) {
+        return match
+      }
+      files[filename] = uri
+      return match.replace(p2, filename)
+    })
+
+    await Promise.map(Object.keys(files), async filename => {
+      try {
+        const resp = await got(files[filename], {
+          encoding: 'binary',
+        })
+        await fs.outputFile(path.join(directory, filename), resp.body, 'binary')
+      } catch (e) {
+        console.error(files[filename], e)
+      }
+    })
+
+    const content = matter.stringify(text, {
+      date: dateFns.format(date),
+      draft: post['wp:status']._cdata === 'draft',
+      tags: [],
+      title: post.title._text,
+    })
+
+    await fs.writeFile(path.join(directory, 'index.md'), content)
+  })
 }
 
 main()
